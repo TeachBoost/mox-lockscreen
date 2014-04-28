1d5faa044fc3e9bf291022dab6691dc1bf6f3e6c
Mike Gioia <mikegioia@gmail.com>
2014-04-27 23:28:12 -0400

    ported jquery methods into this library, hiding loading and setting the lock class

--- index.js
+++ index.js
@@ -2,10 +2,35 @@
  * Lock Screens and Overlays
  */
 
-var Ractive = require( 'ractive/build/ractive.runtime' );
+// dependencies
+var Ractive = require( 'ractive/build/ractive.runtime' ),
+    $ = require( 'jquery' );
 
 // library
 var Lockscreen = {
+    // hide the loading screen, if there is one and if
+    // it's not already hidden.
+    loadingHidden: false,
+    hideLoading: function () {
+        // if it's hidden, return true
+        if ( this.loadingHidden ) {
+            return true;
+        }
+        // get the loading element; if it doesn't exist then
+        // get out.
+        var $loading = $( '#loading' );
+        this.loadingHidden = true;
+        if ( ! $loading.length ) {
+            return true;
+        }
+        // fade the loading screen out
+        $loading.fadeOut( 250 );
+        return true;       
+    },
+    // set the global lock class
+    setLock: function () {
+        $( 'body' ).addClass( 'with-global-lock' );
+    }
     // show the offline screen
     showOffline: function () {
         var View = Ractive.extend({
